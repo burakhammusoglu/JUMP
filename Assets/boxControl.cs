d0a831ad2b308de62105c24b06f57557d86dca91
burakhammusoglu <mburak.hammusoglu@gmail.com>
2020-03-15 17:54:21 +0300

    JumpV.1

---
+++ Assets/boxControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float hiz;
+    Rigidbody2D fizik;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float yatay = Input.GetAxis("Horizontal");
+        
+        transform.position += new Vector3(yatay * hiz * Time.deltaTime, 0);
+        
+        
+    }
+}
